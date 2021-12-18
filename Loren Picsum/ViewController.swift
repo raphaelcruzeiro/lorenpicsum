8b2549668cd0b60e6649c19411bdaa470018b166
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2021-12-18 15:51:01 +0000

    Initial Commit

---
+++ Loren Picsum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loren Picsum
+//
+//  Created by Raphael Cruzeiro on 18/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
