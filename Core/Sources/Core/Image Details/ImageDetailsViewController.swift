bfca11b8fc64a77e73bee879e3a724444fd3e63c
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2021-12-25 23:18:41 +0000

    Added the activity view controller

--- Core/Sources/Core/Image Details/ImageDetailsViewController.swift
+++ Core/Sources/Core/Image Details/ImageDetailsViewController.swift
@@ -10,6 +10,19 @@ import Utils
 
 public final class ImageDetailsViewController: ViewModelViewController<ImageDetailsViewHolder, ImageDetailsViewModel> {
     
+    var image: UIImage?
+    
+    public override func setup() {
+        super.setup()
+        
+        let shareButton = UIBarButtonItem(
+            image: UIImage(systemName: "square.and.arrow.up"),
+            style: .plain, target: self,
+            action: #selector(self.shareTap(_:))
+        )
+        navigationItem.rightBarButtonItem = shareButton
+    }
+    
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         load()
@@ -22,12 +35,20 @@ public final class ImageDetailsViewController: ViewModelViewController<ImageDeta
             do {
                 let imageData = try await viewModel.fetchImage(with: view.bounds.size)
                 let image = UIImage(data: imageData) ?? UIImage()
+                self.image = image
                 hostedView.set(image: image, with: viewModel.item?.author ?? "")
                 hostedView.isLoading = false
             } catch {
                 
             }
         }
+    }
+    
+    @objc private func shareTap(_ sender: UIControl) {
+        let activityController = UIActivityViewController(
+            activityItems: [image as Any], applicationActivities: nil
+        )
+        present(activityController, animated: true, completion: nil)
     }
     
 }
